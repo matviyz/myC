76dd8d5e18cbf05450a8ae88fef8a035658eeb0d
YOUR_FIRST_NAME YOUR_LAST_NAME <YOUR@EMAIL_ADDRESS>
2021-10-07 21:00:03 +0000

    added problem 4

--- HW2/4/myCode.c
+++ HW2/4/myCode.c
@@ -1,5 +1,29 @@
 #include <stdio.h>
 
 int main(void) {
-	printf("Hello, World!\n");
+    float in = 0;
+    int q,d,n,p = 0;    
+    int i;
+
+    scanf("%f", &in);
+    printf("%.2f\n", in);
+
+       for(q = 0;in > 0.25;++q){
+            in = in - 0.25;
+        }
+       for(d = 0;in > 0.10;++d){
+            in = in - 0.10;
+        }
+       for(n = 0;in > 0.05;++n){
+            in = in - 0.05;
+        }
+       while(!(in > 0)){
+            in = in - 0.01;
+            ++p;
+        }
+
+    printf("%d quarters, %d dimes, %d nickles, %d pennies\n", q,d,n,p);
+    printf("%.2f\n", in);
+
+    return 0;
 }
