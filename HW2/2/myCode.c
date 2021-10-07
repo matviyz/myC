cded883c3eb343972193023b13dd520fb245e40d
YOUR_FIRST_NAME YOUR_LAST_NAME <YOUR@EMAIL_ADDRESS>
2021-10-07 17:47:05 +0000

    Added problem 1 and 2

--- HW2/2/myCode.c
+++ HW2/2/myCode.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 
 int main(void) {
-	printf("Hello, World!\n");
+    int in,min = 0;
+    int hr = 0;
+
+    printf("Please input minutes as an integer: ");
+    scanf("%d", &in);
+    min = in;
+
+    for(hr = 0;min >= 60;++hr){
+           min = (min - 60);
+        }
+    
+    printf("%d minutes = %d hour(s) %d minutes.", in,hr,min);
+
+    return 0;
 }
