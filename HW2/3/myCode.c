291ba0c14b6e42b97a8af9ae6091d52830d446a5
Matviy Zhachek <mzhachek3255@bths.edu>
2021-10-12 01:36:12 -0400

    Added problem 3 and formatted problem 4

--- HW2/3/myCode.c
+++ HW2/3/myCode.c
@@ -1,5 +1,22 @@
 #include <stdio.h>
 
 int main(void) {
-	printf("Hello, World!\n");
+    float f = 0; //float input
+    int i = 0; //integer input
+    char c = 0; //character input
+
+    printf("input a letter: "); 
+    scanf("%c", &c); //inputs a character and assigns to char c
+
+    printf("input a decimal number: ");
+    scanf("%f", &f);  //inputs a float and assigns to float f
+
+    printf("input an integer: ");
+    scanf("%d", &i); //inputs an integer and assigns to int i
+
+    printf("your decimal number is %d.\n", f); //prints float using int conversion
+    printf("your integer is %f.\n", i); //prints integer using float conversion
+    printf("your letter is %d.\n", c); //prints letter using integer conversion
+
+    return 0;
 }
