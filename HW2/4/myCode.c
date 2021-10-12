f867492c76b5465b1fba711ef1e0c2397029768e
Matviy Zhachek <mzhachek3255@bths.edu>
2021-10-12 01:14:59 -0400

    Fixed problem 4

--- HW2/4/myCode.c
+++ HW2/4/myCode.c
@@ -1,29 +1,56 @@
 #include <stdio.h>
 
 int main(void) {
-    float in = 0;
-    int q,d,n,p = 0;    
-    int i;
+    float in = 0; //input float
+    int q,d,n,p = 0; //variables for each coin type 
 
-    scanf("%f", &in);
-    printf("%.2f\n", in);
+    //doesn't work because float arithmetic is weird in C
+    /*
+    while (in >= -100){ //temporary infinite loop to test various values
+        scanf("%f", &in); //input a float and place in 'in'
+        printf("%.2f\n", in); //temporary test print the value
 
-       for(q = 0;in > 0.25;++q){
-            in = in - 0.25;
-        }
-       for(d = 0;in > 0.10;++d){
+        for(q = 0;in >= 0.25;++q){ //for loop checks if the vaue is geq coin value
+            in = in - 0.25;        //subtracts the value of the coin
+        }                          //then increments the respective coin
+        for(d = 0;in >= 0.10;++d){
             in = in - 0.10;
         }
-       for(n = 0;in > 0.05;++n){
+        for(n = 0;in >= 0.05;++n){
             in = in - 0.05;
         }
-       while(!(in > 0)){
-            in = in - 0.01;
-            ++p;
+        in = in * 100;
+        for(p = 0;in >= 1;++p){
+            in = in - 1;
         }
 
-    printf("%d quarters, %d dimes, %d nickles, %d pennies\n", q,d,n,p);
-    printf("%.2f\n", in);
+        printf("%d quarters, %d dimes, %d nickles, %d pennies\n", q,d,n,p);
+        //prints the final values of the coins
+        printf("%.2f\n", in); //temporary prints float value to check its 0
+    }*/
+
+    while (in >= -100){ //temporary infinite loop to test various values
+        scanf("%f", &in); //input a float and place in 'in'
+        printf("%.2f\n", in); //temporary test print the value
+
+        in = in * 100; //converts to an int by multiplying by 100
+        for(q = 0;in >= 25;++q){ //for loop checks if the vaue is geq coin value
+            in = in - 25;        //subtracts the value of the coin
+        }                        //then increments the respective coin
+        for(d = 0;in >= 10;++d){
+            in = in - 10;
+        }
+        for(n = 0;in >= 5;++n){
+            in = in - 5;
+        }
+        for(p = 0;in >= 1;++p){
+            in = in - 1;
+        }
+
+        printf("%d quarters, %d dimes, %d nickles, %d pennies\n", q,d,n,p);
+        //prints the final values of the coins
+        printf("%.2f\n", in); //temporary prints float value to check its 0
+    }
 
     return 0;
 }
